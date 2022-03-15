ef44498e5a7f6bc985745133de45986b94d09e7c
kimmyunghoon <roykimmyunghoon@gmail.com>
2022-03-16 08:17:26 +0900

    first commit

---
+++ backend/controller/goalController.js
@@ -0,0 +1,71 @@
+const asyncHandler = require('express-async-handler');
+
+const Goal = require('../models/goalsModel')
+
+// @desc Get goals
+// @route GET /api/goals
+// @access Private
+const getGoals = asyncHandler(async (req,res) => {
+
+    const goals = await Goal.find()
+
+    res.status(200).json(goals);
+})
+
+// @desc Set goals
+// @route Post /api/goals
+// @access Private
+const setGoals = asyncHandler(async (req,res) => {
+
+    console.log(req.body)
+    if(!req.body.text) {
+        throw new Error('Please send a text field');
+        // res.status(400).json({message : 'Please add a text field'});
+    }
+
+    const goal = await Goal.create({
+        text : req.body.text,
+    })
+    res.status(200).json(goal);
+})
+
+// @desc Update goals
+// @route Put /api/goals/:id
+// @access Private
+const updateGoals = asyncHandler(async (req,res) => {
+
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new : true})
+
+    res.status(200).json(updatedGoal);
+})
+
+// @desc Delete goals
+// @route Delete /api/goals/:id
+// @access Private
+const deleteGoals = asyncHandler(async (req,res) => {
+
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove()
+
+    res.status(200).json({ id : req.params.id });
+})
+
+module.exports = {
+    getGoals,
+    setGoals,
+    updateGoals,
+    deleteGoals,
+}
---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { getGoals, setGoals, updateGoals, deleteGoals  } = require('../controller/goalController');
+
+// router.route('/').get(getGoals).post(setGoals)
+
+router.get('/', getGoals );
+
+router.post('/', setGoals);
+
+router.put('/:id', updateGoals);
+
+router.delete('/:id', deleteGoals);
+
+
+module.exports = router
